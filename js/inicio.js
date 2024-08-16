11810542387bafd92fadf6a7347cffcc19b3843e
Rodrigo Laplacette <rodrigolaplacette@yahoo.com.ar>
2024-08-16 01:43:15 -0300

    traducciones

---
+++ js/inicio.js
@@ -0,0 +1,105 @@
+const translations = {
+    es: {
+
+    bsobremi: "SOBRE MI",
+    bexperiencia: "EXPERIENCIA",
+    bproyecto: "PROYECTOS",
+    bcontacto: "CONTACTO",
+
+    txtinicio: "Bienvenidos a mi espacio digital. Soy Rodrigo Laplacette, un profesional con una visión clara: combinar mi pasión por la tecnología y el automovilismo para crear soluciones innovadoras. Con años de experiencia en informática, diseño 3D, y un profundo amor por el automovilismo, estoy listo para dar el siguiente paso en mi carrera. Explora mi trabajo y descubre lo que me impulsa cada día.",
+    
+    titlebio: "Biografía",
+    txtsobremi:"Tengo 31 años(1993) y nací el 18 de febrero, compartiendo fecha de nacimiento con mi ídolo, Enzo Ferrari. Mi carrera comenzó con una década de experiencia en el manejo de autoelevadores, donde aprendí la importancia del trabajo en equipo, la precisión, y la responsabilidad. A lo largo de mi vida, la informática ha sido una constante, permitiéndome dominar diversos sistemas operativos, junto con una profunda comprensión del hardware y software, incluyendo inteligencia artificial y tecnología de realidad virtual. Además, soy autodidacta en programación, con conocimientos avanzados en HTML5, CSS, SASS, y en el uso de herramientas como Git Bash, las cuales utilizo para desarrollar esta misma página web. También tengo experiencia en el manejo de todas las redes sociales, lo que me permite estar conectado y al día con las tendencias digitales.",
+    titlepasion:"Pasión y Aspiraciones:",
+    txtmipasion:"Mi pasión por Ferrari y la Fórmula 1 ha guiado mis pasos hacia el diseño 3D y la impresión 3D, donde he emprendido mi propio negocio, especializándome en artículos relacionados con el automovilismo. Actualmente, me encuentro perfeccionando mis habilidades en modelado 3D a través del estudio de CATIA V5, con la meta de llevar mi amor por la velocidad y la tecnología al siguiente nivel.",
+    titleexp:"EXPERIENCIA PROFESIONAL",
+    titleoperario:"Operador de Autoelevadores:",
+    txtoperario:"Durante 10 años, trabajé en la empresa Quilmes Cerveza (AB-InBev), manejando autoelevadores de hasta 8 horquillas y 8 mil toneladas. Además, adquirí habilidades en el mantenimiento general de estos vehículos, asegurando operaciones seguras y eficientes.",
+    titleinformatica:"Informática y Tecnología:",
+    txtinformatica:"Como experto en informática, manejo una amplia gama de sistemas operativos y soy competente en el uso del paquete Office. También tengo habilidades en la reparación y montaje de PCs, y una capacidad innata para aprender y adaptarme a nuevas tecnologías, incluyendo inteligencia artificial. Además, tengo experiencia en programación web utilizando HTML5, CSS, Javascript, SASS, y en el uso de herramientas como Git Bash para gestionar proyectos de desarrollo. Estoy familiarizado con todas las redes sociales, lo que me permite utilizar estas plataformas para fortalecer mi presencia en línea y conectar con audiencias diversas. Actualmente, estoy desarrollando mi propia página web como un proyecto personal.",
+    titleemprendimiento:"Emprendimiento en Impresión 3D:",
+    txtemprendimiento:"Llevo 3 años como emprendedor en la fabricación de artículos impresos en 3D relacionados con el automovilismo. Manejo programas de laminado 3D como Cura slicer, Orca slicer y Prusa slicer, y tengo experiencia en la reparación y armado de impresoras 3D de diferentes tipos.",
+    titleimpresion:"Impresión 3D y Diseño:",
+    txtminegocio:"Mi negocio de impresión 3D se enfoca en la creación de piezas y objetos dedicadas especialmente al automovilismo. Desde el diseño básico en Fusion 360, Catia V5 o Tinkercad, hasta la impresión final. Cada proyecto es una demostración de mi atención al detalle y mi búsqueda de la perfección.",
+    titlesimracing:"Simracing",
+    txtsimracing:"Soy un apasionado del Simracing y tengo un profundo conocimiento de la configuración de simuladores y tecnología de realidad virtual. Mi experiencia abarca casi todas las pistas del mundo virtual, lo que me permite perfeccionar mis habilidades al máximo.",
+    titlegaleria:"MIS PROYECTOS - IMPRESIÓN Y DISEÑO 3D",
+
+
+
+    },
+
+
+
+
+    en: {
+
+    bsobremi: "ABOUT ME",
+    bexperiencia: "EXPERIENCE",
+    bproyecto: "PROJECTS",
+    bcontacto: "CONTACT",
+
+    txtinicio: "Welcome to my digital space. I am Rodrigo Laplacette, a professional with a clear vision: to combine my passion for technology and motorsport to create innovative solutions. With years of experience in IT, 3D design, and a deep love for motorsports, I am ready to take the next step in my career. Explore my work and discover what drives me every day.",
+
+    titlebio: "Biography",
+    txtsobremi:"I am 31 years old (1993) and was born on February 18th, sharing a birthdate with my idol, Enzo Ferrari. My career began with a decade of experience in forklift operation, where I learned the importance of teamwork, precision, and responsibility. Throughout my life, IT has been a constant, allowing me to master various operating systems, along with a deep understanding of hardware and software, including artificial intelligence and virtual reality technology. Additionally, I am self-taught in programming, with advanced knowledge of HTML5, CSS, SASS, and tools like Git Bash, which I use to develop this very website. I also have experience managing all social media, keeping me connected and up-to-date with digital trends.",
+    titlepasion:"Passion and Aspirations",
+    txtmipasion:"My passion for Ferrari and Formula 1 has guided my steps towards 3D design and 3D printing, where I have started my own business, specializing in motorsport-related items. Currently, I am perfecting my 3D modeling skills through the study of CATIA V5, with the goal of taking my love for speed and technology to the next level.",
+    titleexp:"Professional Experience",
+    titleoperario:"Forklift Operator",
+    txtoperario:"For 10 years, I worked at Quilmes Cerveza (AB-InBev), operating forklifts with capacities of up to 8 forks and 8 thousand tons. Additionally, I acquired skills in the general maintenance of these vehicles, ensuring safe and efficient operations.",
+    titleinformatica:"IT and Technology:",
+    txtinformatica:"As an IT expert, I handle a wide range of operating systems and am proficient in using the Office suite. I also have skills in PC repair and assembly, with an innate ability to learn and adapt to new technologies, including artificial intelligence. Furthermore, I have experience in web programming using HTML5, CSS, Javascript, SASS, and tools like Git Bash for managing development projects. I am familiar with all social media platforms, allowing me to use these platforms to strengthen my online presence and connect with diverse audiences. Currently, I am developing my own website as a personal project.",
+    titleemprendimiento:"3D Printing Entrepreneurship",
+    txtemprendimiento:"I have 3 years of experience as an entrepreneur in the production of 3D-printed items related to motorsports. I manage 3D slicing programs like Cura slicer, Orca slicer, and Prusa slicer, and have experience in repairing and assembling various types of 3D printers.",
+    titleimpresion:"Projects - 3D Printing and Design",
+    txtminegocio:"My 3D printing business focuses on creating parts and objects specifically dedicated to motorsport. From basic design in Fusion 360, Catia V5, or Tinkercad to the final print, each project is a demonstration of my attention to detail and my pursuit of perfection.",
+    titlesimracing:"Simracing",
+    txtsimracing:"I am passionate about Simracing and have a deep understanding of simulator setup and virtual reality technology. My experience covers almost every track in the virtual world, allowing me to hone my skills to the fullest.",
+    titlegaleria:"MY PROJECTS - 3D PRINTING AND DESIGN",
+
+    },
+
+
+
+
+    it: {
+
+    bsobremi: "CHI SONO",
+    bexperiencia: "ESPERIENZA",
+    bproyecto: "PROGETTI",
+    bcontacto: "CONTATTO",
+
+    txtinicio:"Benvenuti nel mio spazio digitale. Sono Rodrigo Laplacette, un professionista con una visione chiara: combinare la mia passione per la tecnologia e l'automobilismo per creare soluzioni innovative. Con anni di esperienza nell'informatica, nel design 3D e un profondo amore per l'automobilismo, sono pronto a fare il passo successivo nella mia carriera. Esplora il mio lavoro e scopri cosa mi spinge ogni giorno.",
+
+    titlebio: "Biografia",
+    txtsobremi:"Ho 31 anni (1993) e sono nato il 18 febbraio, condividendo la data di nascita con il mio idolo, Enzo Ferrari. La mia carriera è iniziata con un decennio di esperienza nella gestione di carrelli elevatori, dove ho imparato l'importanza del lavoro di squadra, della precisione e della responsabilità. Nel corso della mia vita, l'informatica è stata una costante, permettendomi di padroneggiare diversi sistemi operativi, insieme a una profonda comprensione dell'hardware e del software, inclusa l'intelligenza artificiale e la tecnologia di realtà virtuale. Inoltre, sono autodidatta nella programmazione, con conoscenze avanzate in HTML5, CSS, SASS e nell'uso di strumenti come Git Bash, che utilizzo per sviluppare questo stesso sito web. Ho anche esperienza nella gestione di tutti i social media, il che mi permette di rimanere connesso e aggiornato sulle tendenze digitali.",
+    titlepasion:"Passione e Aspirazioni",
+    txtmipasion:"La mia passione per la Ferrari e la Formula 1 ha guidato i miei passi verso il design 3D e la stampa 3D, dove ho avviato la mia attività, specializzandomi in articoli legati all'automobilismo. Attualmente, sto perfezionando le mie competenze nel modellismo 3D attraverso lo studio di CATIA V5, con l'obiettivo di portare il mio amore per la velocità e la tecnologia al livello successivo.",
+    titleexp:"Esperienza Professionale",
+    titleoperario:"Operatore di Carrelli Elevatori",
+    txtoperario:"Per 10 anni, ho lavorato presso l'azienda Quilmes Cerveza (AB-InBev), gestendo carrelli elevatori con capacità fino a 8 forche e 8 mila tonnellate. Inoltre, ho acquisito competenze nella manutenzione generale di questi veicoli, garantendo operazioni sicure ed efficienti.",
+    titleinformatica:"Informatica e Tecnologia",
+    txtinformatica:"Come esperto di informatica, gestisco una vasta gamma di sistemi operativi e sono competente nell'uso del pacchetto Office. Inoltre, ho competenze nella riparazione e assemblaggio di PC e una capacità innata di imparare e adattarmi a nuove tecnologie, inclusa l'intelligenza artificiale. Ho anche esperienza nella programmazione web utilizzando HTML5, CSS, Javascript, SASS e nell'uso di strumenti come Git Bash per gestire progetti di sviluppo. Sono familiare con tutti i social media, il che mi consente di utilizzare queste piattaforme per rafforzare la mia presenza online e connettermi con un pubblico diversificato. Attualmente, sto sviluppando il mio sito web come progetto personale.",
+    titleemprendimiento:"Imprenditoria nella Stampa 3D",
+    txtemprendimiento:"Ho 3 anni di esperienza come imprenditore nella produzione di articoli stampati in 3D legati all'automobilismo. Gestisco programmi di slicing 3D come Cura slicer, Orca slicer e Prusa slicer, e ho esperienza nella riparazione e assemblaggio di stampanti 3D di diversi tipi.",
+    titleimpresion:"Stampa 3D e Design",
+    txtminegocio:"La mia attività di stampa 3D si concentra sulla creazione di pezzi e oggetti dedicati in particolare all'automobilismo. Dal design di base in Fusion 360, Catia V5 o Tinkercad fino alla stampa finale. Ogni progetto è una dimostrazione della mia attenzione ai dettagli e della mia ricerca della perfezione.",
+    titlesimracing:"Simracing",
+    txtsimracing:"Sono un appassionato di Simracing e ho una profonda conoscenza della configurazione dei simulatori e della tecnologia di realtà virtuale. La mia esperienza copre quasi tutte le piste del mondo virtuale, il che mi consente di perfezionare le mie abilità al massimo.",
+    titlegaleria:"I MIEI PROGETTI - STAMPA E DESIGN 3D",
+
+
+    }
+};
+
+
+function changeLanguage(language) {
+    const elementsToTranslate = document.querySelectorAll("[data-translate]");
+    
+    elementsToTranslate.forEach(element => {
+    const translationKey = element.getAttribute("data-translate");
+    element.textContent = translations[language][translationKey];
+    });
+}
+
